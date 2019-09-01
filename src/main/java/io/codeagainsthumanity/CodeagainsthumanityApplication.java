08e3f486c4a13026c486b5a76faaae6cc7a0a057
Jane Hur <janehur8@gmail.com>
2019-09-01 00:55:49 -0700

    Basic user account creation

--- src/main/java/io/codeagainsthumanity/CodeagainsthumanityApplication.java
+++ src/main/java/io/codeagainsthumanity/CodeagainsthumanityApplication.java
@@ -2,12 +2,16 @@ package io.codeagainsthumanity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodeagainsthumanityApplication {
+public class CodeagainsthumanityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeagainsthumanityApplication.class, args);
+		System.out.println("http://localhost:8080/");
+		System.out.println("http://localhost:8080/login");
+		System.out.println("http://localhost:8080/registration");
 	}
 
 }
---
+++ src/main/java/io/codeagainsthumanity/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package io.codeagainsthumanity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/registration", "/login", "/error", "/bootstrap.css", "/bootstrap.css.map").permitAll()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/profile")
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .and()
+                .logout();
+
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
