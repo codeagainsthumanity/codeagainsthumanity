b19dfda77b39fee7ebe2f081dff5198f6da344bc
Jack Daniel Kinne <bravelemming@gmail.com>
2019-09-05 19:35:48 -0700

    added to playwhitecard route.  current status is broken on a gameCode double.

--- src/main/java/io/codeagainsthumanity/controllers/DecksController.java
+++ src/main/java/io/codeagainsthumanity/controllers/DecksController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DecksController {
@@ -58,9 +60,30 @@ public class DecksController {
         WhiteCard wc = new WhiteCard(choice);
         game.getToBeJudged().add(wc);
         // player hand submitted is true and their hand/form is hidden until judged
+        Boolean bool = true;
+        game.setBooleanToSubmitted(user.getId(), bool);
 
+        //player drops that card from their hand
 
+        //user draws new card
+        List<String> hand = new ArrayList<>();
+        for (int i = 0 ; i < 7 ; i ++){
+            String cardString = game.getHands().get(user.getId()).get(i);
+            if (!cardString.equals(choice)){
+                hand.add(cardString);
+            }
+            else {
+                WhiteCard random = game.randomWhiteCard();
+                String randomString = random.getText();
+                hand.add(randomString);
+            }
+        }
 
+        //then push hand into games hashmap, called hands.
+        game.getHands().put(user.getId(),hand);
+        //save repos
+        applicationUserRepository.save(user);
+        gameRepository.save(game);
 
 
         return new RedirectView("/game/" + gameCode);
